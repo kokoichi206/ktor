78a89f001ec54e97c5cf6460de1548feeba41726
kokoichi206 <tt.300607@gmail.com>
2021-11-27 21:41:55 +0900

    create data classes

--- ktorsocialnetwork/src/main/kotlin/com/exaaaample/Application.kt
+++ ktorsocialnetwork/src/main/kotlin/com/exaaaample/Application.kt
@@ -3,6 +3,7 @@ package com.exaaaample
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.exaaaample.plugins.*
+import kotlinx.coroutines.GlobalScope
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
